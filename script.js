4fbee5c79e45b0d715d8b4845f360060fd407d48
YuraForlita <forlitaura@gmail.com>
2025-07-04 12:40:13 +0300

    allpage

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-
-  const translations = {
-    ua: {
-      "nav.home": "Головна",
-      "nav.about": "Про нас",
-      "nav.brands": "Бренди",
-      "nav.contacts": "Контакти",
-      "home.greeting": "Привіт , це UTС <span>VERTIKAL</span>",
-      "home.subtitle.static": "Ми є дистриб'ютор",
-      "home.description": "Ми займаємось дистрибуцією побутової техніки, електроніки, бананів і може ще чогось.",
-      "home.more": "Детальніше про нас",
-      "about.title" : "Хто ми такі?",
-      "about.text1" : "UTS VERTIKAL — це команда професіоналів...",
-      "about.text2" : "Ми прагнемо стати надійним партнером...",
-      "about.text3" : "Наші цінності — це прозорість...",
-      "about.contactBtn" : "Зв'язатися з нами"
-    },
-    en: {
-      "nav.home": "Home",
-      "nav.about": "About Us",
-      "nav.brands": "Brands",
-      "nav.contacts": "Contacts",
-      "home.greeting": "Hello, this is UTS <span>VERTIKAL</span>",
-      "home.subtitle.static": "We are a distributor of",
-      "home.description": "We distribute household appliances, electronics, bananas and maybe something else.",
-      "home.more": "More about us"
-    }
-  };
-
-  function setLanguage(lang) {
+function setLanguage(lang) {
     const elements = document.querySelectorAll("[data-i18n]");
     elements.forEach(el => {
       const key = el.getAttribute("data-i18n");
@@ -121,3 +92,9 @@ function toggleMenu() {
       document.getElementById('lang-menu').classList.remove('active');
     }
   }); 
+
+  document.addEventListener("DOMContentLoaded", () => {
+  const savedLang = localStorage.getItem("lang") || "ua";
+  setLanguage(savedLang);
+  document.getElementById('current-lang').innerText = savedLang.toUpperCase(); // <- ось це додаємо
+});
---
+++ translations.js
@@ -0,0 +1,27 @@
+const translations = {
+    ua: {
+      "nav.home": "Головна",
+      "nav.about": "Про нас",
+      "nav.brands": "Бренди",
+      "nav.contacts": "Контакти",
+      "home.greeting": "Привіт , це UTС <span>VERTIKAL</span>",
+      "home.subtitle.static": "Ми є дистриб'ютор",
+      "home.description": "Ми займаємось дистрибуцією побутової техніки, електроніки, бананів і може ще чогось.",
+      "home.more": "Детальніше про нас",
+      "about.title" : "Хто ми такі?",
+      "about.text1" : "UTS VERTIKAL — це команда професіоналів...",
+      "about.text2" : "Ми прагнемо стати надійним партнером...",
+      "about.text3" : "Наші цінності — це прозорість...",
+      "about.contactBtn" : "Зв'язатися з нами"
+    },
+    en: {
+      "nav.home": "Home",
+      "nav.about": "About Us",
+      "nav.brands": "Brands",
+      "nav.contacts": "Contacts",
+      "home.greeting": "Hello, this is UTS <span>VERTIKAL</span>",
+      "home.subtitle.static": "We are a distributor of",
+      "home.description": "We distribute household appliances, electronics, bananas and maybe something else.",
+      "home.more": "More about us"
+    }
+  };
