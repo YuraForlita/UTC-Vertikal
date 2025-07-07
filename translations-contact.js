febfeb227dce9f26d0520b0c6dcd677c95e30412
YuraForlita <forlitaura@gmail.com>
2025-07-07 09:51:39 +0300

    ,

---
+++ translations-contact.js
@@ -0,0 +1,25 @@
+const translations = {
+  ua: {
+    "nav.home": "Головна",
+    "nav.about": "Про нас",
+    "nav.brands": "Бренди",
+    "nav.contacts": "Контакти",
+    "contacts.title": "Наші контакти",
+    "form.name": "Імʼя",
+    "form.email": "Електронна пошта",
+    "form.message": "Повідомлення",
+    "form.submit": "Надіслати"
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About Us",
+    "nav.brands": "Brands",
+    "nav.contacts": "Contacts",
+    "contacts.title": "Contact Us",
+    "form.name": "Name",
+    "form.email": "Email",
+    "form.message": "Message",
+    "form.submit": "Send"
+  }
+};
+
