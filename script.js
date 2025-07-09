d4d152cbb2e73c5d0a0c2eeec50741a824efe615
YuraForlita <forlitaura@gmail.com>
2025-07-09 10:41:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -110,3 +110,27 @@ function toggleMenu() {
       });
     }
   });
+
+  const counters = document.querySelectorAll(".counter");
+const speed = 300;
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 20);
+    } else {
+      counter.innerText = target.toLocaleString("en-US");
+    }
+  };
+
+  const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) updateCount();
+  }, { threshold: 1 });
+
+  observer.observe(counter);
+});
